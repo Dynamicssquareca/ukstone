faec20b04758608f1af889d9a9a10154c8109088
Dynamicssquareca <dynamicssquareca@gmail.com>
2025-04-23 15:54:15 +0530

    updatecode

--- next.config.mjs
+++ next.config.mjs
@@ -4,26 +4,26 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: true,
   output: "standalone",
-  env: {
-    NEXT_PUBLIC_SITE_URL:'https://www.crmfrontier.com/',
-    NEXT_PUBLIC_BLOG_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blogs',
-    NEXT_PUBLIC_BLOG_PREVIEW_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blogs/preview',
-    NEXT_PUBLIC_CATEGORY_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blog/categories',
-    NEXT_PUBLIC_AUTHOR_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blog/authors',
-    NEXT_PUBLIC_AUTHOR_BLOG_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blogs/author/',
-    NEXT_PUBLIC_BLOG_API_Image:'https://crmfoceplus-backend.onrender.com/uploads',
-    NEXT_PUBLIC_BLOG_API_Image_profilePics:'https://crmfoceplus-backend.onrender.com/uploads'
-  },
+  // env: {
+  //   NEXT_PUBLIC_SITE_URL:'https://www.crmfrontier.com/',
+  //   NEXT_PUBLIC_BLOG_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blogs',
+  //   NEXT_PUBLIC_BLOG_PREVIEW_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blogs/preview',
+  //   NEXT_PUBLIC_CATEGORY_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blog/categories',
+  //   NEXT_PUBLIC_AUTHOR_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blog/authors',
+  //   NEXT_PUBLIC_AUTHOR_BLOG_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blogs/author/',
+  //   NEXT_PUBLIC_BLOG_API_Image:'https://crmfoceplus-backend.onrender.com/uploads',
+  //   NEXT_PUBLIC_BLOG_API_Image_profilePics:'https://crmfoceplus-backend.onrender.com/uploads'
+  // },
   images: {
     formats: ["image/avif", "image/webp"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "crmfoceplus-backend.onrender.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "crmfoceplus-backend.onrender.com",
+    //     port: "",
+    //     pathname: "/**",
+    //   },
+    // ],
     deviceSizes: [320, 420, 768, 1024, 1200],
     imageSizes: [16, 32, 48, 64, 96],
   },
--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,15 @@
 import React, { useState,useRef } from 'react';
-import ServiceBox from "@/components/ServiceBox";
-import Head from "next/head";
-import Image from "next/image";
-// import { Inter } from "next/font/google";
-// import styles from "@/styles/Home.module.css";
-import { TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap';
-import ModelBox from '@/components/ModelBox';
-import { Swiper, SwiperSlide } from 'swiper/react';
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import { Navigation, Autoplay } from 'swiper/modules';
-// const inter = Inter({ subsets: ["latin"] });
+import Head from 'next/head';
 
 export default function Home() {
 
-  const [activeTab, setActiveTab] = useState('1');
 
-  const toggle = tab => {
-    if (activeTab !== tab) setActiveTab(tab);
-  };
 
 
   return (
     <>
       <Head>
-        <title>Trusted Salesforce Implementation Partner in USA | CRM Frontier</title>
-        <meta name="description" content="CRM Frontier is a trusted Salesforce consulting and implementation partner in USA, offering expert solutions to optimize your business processes and drive success with Salesforce CRM. Contact us today!" />
-        <link rel="canonical" href="https://www.crmfrontier.com/" />
-        <meta property="og:locale" content="US" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Trusted Salesforce Implementation Partner in USA | CRM Frontier" />
-        <meta property="og:description" content="CRM Frontier is a trusted Salesforce consulting and implementation partner in USA, offering expert solutions to optimize your business processes and drive success with Salesforce CRM. Contact us today!" />
-        <meta property="og:url" content="https://www.crmfrontier.com/" />
-        <meta property="og:site_name" content="CRM Frontier " />
-        <meta property="og:image" content="https://www.crmfrontier.com/img/feature/Homepage.jpg" />
-        <meta property="og:image:width" content="200" />
-        <meta property="og:image:height" content="200" />
-        <meta property="og:image:type" content="image/jpg" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@CRM Frontier" />
-        <meta name="twitter:title" content="Trusted Salesforce Implementation Partner in USA | CRM Frontier" />
-        <meta name="twitter:description" content="CRM Frontier is a trusted Salesforce consulting and implementation partner in USA, offering expert solutions to optimize your business processes and drive success with Salesforce CRM. Contact us today!" />
-        <meta name="twitter:image" content="https://www.crmfrontier.com/img/feature/Homepage.jpg" />
-
+        <title>Home Page</title>
       </Head>
       
 
