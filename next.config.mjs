b7e4304df6131f416fe6f9b9cf35249303060e47
Dynamicssquareca <dynamicssquareca@gmail.com>
2025-05-12 20:22:28 +0530

    update code

--- next.config.mjs
+++ next.config.mjs
@@ -12,14 +12,14 @@ const nextConfig = {
     NEXT_PUBLIC_CATEGORY_API_URL:'https://stonediscoveruk-apibackend.onrender.com/api/frontend/categories',
     NEXT_PUBLIC_CATEGORY_CHILD_API_URL:'https://stonediscoveruk-apibackend.onrender.com/api/frontend/productscategory',
     NEXT_PUBLIC_DETSILS_URL:'https://stonediscoveruk-apibackend.onrender.com/api/frontend/categorydetails',
-    NEXT_PUBLIC_IMAGE:'https://stonediscoveruk-apibackend.onrender.com/uploads'
-    // NEXT_PUBLIC_BLOG_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blogs',
-    // NEXT_PUBLIC_BLOG_PREVIEW_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blogs/preview',
-    // NEXT_PUBLIC_CATEGORY_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blog/categories',
-    // NEXT_PUBLIC_AUTHOR_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blog/authors',
-    // NEXT_PUBLIC_AUTHOR_BLOG_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blogs/author/',
-    // NEXT_PUBLIC_BLOG_API_Image:'https://crmfoceplus-backend.onrender.com/uploads',
-    // NEXT_PUBLIC_BLOG_API_Image_profilePics:'https://crmfoceplus-backend.onrender.com/uploads'
+    NEXT_PUBLIC_IMAGE:'https://stonediscoveruk-apibackend.onrender.com/uploads',
+    NEXT_PUBLIC_BLOG_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blogs',
+    NEXT_PUBLIC_BLOG_PREVIEW_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blogs/preview',
+    NEXT_PUBLIC_CATEGORY_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blog/categories',
+    NEXT_PUBLIC_AUTHOR_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blog/authors',
+    NEXT_PUBLIC_AUTHOR_BLOG_API_URL:'https://crmfoceplus-backend.onrender.com/api/frontend/blogs/author/',
+    NEXT_PUBLIC_BLOG_API_Image:'https://crmfoceplus-backend.onrender.com/uploads',
+    NEXT_PUBLIC_BLOG_API_Image_profilePics:'https://crmfoceplus-backend.onrender.com/uploads'
   },
   images: {
     formats: ["image/avif", "image/webp"],
@@ -27,10 +27,12 @@ const nextConfig = {
       {
         protocol: "https",  
         hostname: "stonediscoveruk-apibackend.onrender.com",
+        hostname: "crmfoceplus-backend.onrender.com",
         port: "",
         pathname: "/**",
       },
     ],
+    domains: ['cdn.dummyjson.com'],
     deviceSizes: [320, 420, 768, 1024, 1200],
     imageSizes: [16, 32, 48, 64, 96],
   },
