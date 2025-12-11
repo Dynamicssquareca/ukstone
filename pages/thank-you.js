8e5896fda8768dc3106dcae310948a2c1f8d9c08
dynamicssquareca <dynamicssquareca@gmail.com>
2025-12-11 13:30:57 +0530

    farma

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -1,33 +1,37 @@
-import Head from 'next/head';
-import React from 'react';
+import Head from "next/head";
+import React from "react";
 const ThankYou = () => {
-    return (
-        <>
-        <Head>
-            <title>Thank You | CRM Frontier</title>
-            <meta
-                    name="description"
-                    content="Thanks for getting in touch - someone will contact you soon!"
-                />
-            <link rel="canonical" href="https://www.crmfrontier.com/thank-you/" />
-        </Head>
+  return (
+    <>
+      <Head>
+        <title>Thank You | CRM Frontier</title>
+        <meta
+          name="description"
+          content="Thanks for getting in touch - someone will contact you soon!"
+        />
+        <link rel="canonical" href="https://www.crmfrontier.com/thank-you/" />
+      </Head>
 
-        <section className="hero">
+      <section className="hero">
         <div id="hero"></div>
         <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-lg-9">
-                    <div className="hero-content">
-                        <h1>Thank You !</h1>
-                       <p>Thanks for getting in touch - someone will contact you soon!</p>
-                       <a className="btn btn-one" href='/'>Back to Home</a>
-                    </div>
-                </div>
+          <div className="row justify-content-center">
+            <div className="col-lg-9">
+              <div className="hero-content">
+                <h1>Thank You !</h1>
+                <p>
+                  Thanks for getting in touch - someone will contact you soon!
+                </p>
+                <a className="btn btn-one" href="/">
+                  Back to Home
+                </a>
+              </div>
             </div>
+          </div>
         </div>
-    </section>
+      </section>
     </>
-    );
-}
+  );
+};
 
 export default ThankYou;
